9055e50068800541a8785ccb546498860e823e21
Boris Kamstra <kams0007@hz.nl>
2021-12-03 15:32:48 +0100

    set the game in a different div than the model

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -318,15 +318,12 @@
                             data-aos="fade-in"
                             data-aos-easing="linear">
                         </div>
-
                         <div class="container">
-
                                 @php
                                     @$useragent=$_SERVER['HTTP_USER_AGENT'];
                                 @endphp
-
+{{--                            Decide if the visitor is using a PC or mobile device--}}
                                 @if(preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i',$useragent)||preg_match('/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i',substr($useragent,0,4)))
-                                    phone
                                     <div class="columns is-mobile is-centered">
                                         <iframe style="width:300px; height:300px;"
                                                 src='https://my.matterport.com/show/?m=5RM24QU21q4'
@@ -334,7 +331,6 @@
                                                 allow='xr-spatial-tracking' class="column is-centered">
                                          </iframe>
                                 @else
-                                     no phone
                                     <div class="columns is-centered">
                                         <iframe style="width:700px; height:480px;"
                                                 src='https://my.matterport.com/show/?m=5RM24QU21q4'
@@ -347,28 +343,21 @@
                 </div>
             </div>
 
-            <!-- Begin of the Game -->
-            <div>
-                <div class="column is-12 about-me" id="game">
-                    <br><br><br>
-                    <h1 class="title has-text-centered section-title">Game</h1>
-                </div>
-                <div class="container">
-                    <div class="columns is-centered">
-                        <a href="https://games.gdevelop-app.com/game-8b2f3e0f-a627-46e6-ac54-2163546a66db/index.html" target="_blank">
-                            <img src="/images/Logo.png" class="is-hoverable"></a>
-                    </div>
-                </div>
-            </div>
-
-
-
-
-
-
-
             <!-- End Services Content -->
         </div>
+                <!-- Begin of the Game -->
+                <div>
+                    <div class="column is-12 about-me" id="game">
+                        <br><br><br>
+                        <h1 class="title has-text-centered section-title">Game</h1>
+                    </div>
+                    <div class="container">
+                        <div class="columns is-centered">
+                            <a href="https://games.gdevelop-app.com/game-8b2f3e0f-a627-46e6-ac54-2163546a66db/index.html" target="_blank">
+                                <img src="/images/Logo.png" class="is-hoverable"></a>
+                        </div>
+                    </div>
+                </div>
         <!-- End Main Content -->
 @endsection
 
