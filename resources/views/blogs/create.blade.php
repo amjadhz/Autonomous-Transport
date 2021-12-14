86c0f53285359ceb6789b0e6f2c233cdf1074563
amjadhz <hwid0001@hz.nl>
2021-12-14 10:11:58 +0100

    Update create.blade.php

--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -49,7 +49,6 @@
                                 <label for="image" class="col-md-4 col-form-label text-md-right">Image</label>
 
                                 <div class="col-md-6">
-
                                     <input type="file" id="image" name="image" accept="image/*" value="{{old('image')}}">
                                 </div>
                             </div>
