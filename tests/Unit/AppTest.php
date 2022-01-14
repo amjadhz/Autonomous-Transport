6c94380760d42c2171a2eb1362dfe3f211120ced
amjadhz <hwid0001@hz.nl>
2022-01-14 13:02:15 +0100

    Add unit test

---
+++ tests/Unit/AppTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Http\Controllers\UserController;
+use App\Models\User;
+use Database\Seeders\UserSeerder;
+use Illuminate\Database\Seeder;
+use Tests\TestCase;
+
+class AppTest extends TestCase
+{
+
+    public function test_get_index(){
+        $response = $this->get('/');
+        $response->assertStatus(200);
+    }
+
+    public function test_create_users(){
+        $user1 = User::make([
+            'name'=> 'Admin1',
+            'email'=>'admin1@admin.com',
+            'password'       => bcrypt('password'),
+            'remember_token' => null,
+        ]);
+        $user2 = User::make([
+            'name'=> 'Admin2',
+            'email'=>'admin2@admin.com',
+            'password'       => bcrypt('password'),
+            'remember_token' => null,
+        ]);
+
+        $this->assertTrue($user1->name!=$user2->name);
+        $this->assertTrue($user1->email!=$user2->email);
+    }
+}
